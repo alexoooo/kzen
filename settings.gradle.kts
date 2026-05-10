2c1f12a57acf18a19b6723cf386b521ec3f0fe22
alexoooo <alex@ostrovsky.biz>
2026-05-09 20:41:16 -0400

    project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "kzen"
+includeBuild("../kzen-lib")
+includeBuild("../kzen-auto")
+includeBuild("../kzen-project")
+includeBuild("../kzen-launcher")
+includeBuild("../kzen-shell")
